d54dacc5a436579c26d947251d7b852439708777
yuya-toyoizumi <gbr.eng123@gmail.com>
2023-12-24 22:05:20 +0900

    add sample data fetch

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,26 @@
-export default function Page() {
+async function getDogList() {
+  const res = await fetch(`${process.env.API_URL}/dogs`);
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data!');
+  }
+
+  return res.json();
+}
+
+export default async function Page() {
+  const dogList = await getDogList();
+  
   return (
-  <h1>Hello, Dashboard Page!</h1>
+    <>
+      <h1>Hello, Dashboard Page!</h1>
+      <ul>
+        {dogList.map((dog) => {
+          return (
+            <li key={dog.id}>{dog.name}</li>
+          )
+        })}
+      </ul>
+    </>
   )
 }
